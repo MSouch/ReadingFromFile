81415d1172df2405a1edf3d586ffd7e8395f0e02
MSouch <msouch3@gmail.com>
2024-01-30 09:05:20 -0700

    Add project files.

---
+++ ReadingFromFile/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab2Review;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("hello world");
+        string[] students = File.ReadAllLines("C:\\Users\\msouc\\source\\repos\\ReadingFromFile\\ReadingFromFile\\TextFile1.txt");
+        foreach (string student in students)
+        {
+            Console.WriteLine(student);
+            string[] studentData = student.Split(',');
+            foreach(string data in studentData)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
